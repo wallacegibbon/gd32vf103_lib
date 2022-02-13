982561411145f216e41f4ac9020f6614cf0211c2
wallace <wallacegibbon@outlook.com>
2022-02-13 22:12:02 +0800

    compiles successfully, assembly files are all removed, the crt0 of the compiler is used instead.

--- test/src/main.c
+++ test/src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+char my_variable[] = "hello, world!";
+char buf[100];
+
 int main(int argc, const char **argv) {
-	printf("hello~\n");
+	for (int i = 0; i < 100; i++)
+		buf[i] = my_variable[i % 10];
+
 	return 0;
 }
