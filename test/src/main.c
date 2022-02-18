d25e9c2a8a63677332583d486f25495512cbfeab
wallace <wallacegibbon@outlook.com>
2022-02-19 00:55:27 +0800

    the bug in main.c got fixed, usart0 transmitting is working now, but the interrupt is still not working yet.

--- test/src/main.c
+++ test/src/main.c
@@ -6,20 +6,23 @@ char buf[100];
 
 __attribute__((interrupt))
 void USART0_IRQn_handler() {
-	//gpio_bit_reset(GPIOC, GPIO_PIN_13);
+	gpio_bit_reset(GPIOC, GPIO_PIN_13);
 	if (usart_interrupt_flag_get(USART0, USART_INT_FLAG_RBNE) != RESET) {
 		gpio_bit_reset(GPIOA, GPIO_PIN_1);
-	} else {
+	}
+	if (usart_interrupt_flag_get(USART0, USART_INT_FLAG_TBE) != RESET) {
 		gpio_bit_reset(GPIOA, GPIO_PIN_2);
 	}
 }
 
 void init() {
+	system_init();
+
 	rcu_periph_clock_enable(RCU_GPIOA);
 	rcu_periph_clock_enable(RCU_GPIOC);
 	rcu_periph_clock_enable(RCU_USART0);
 
-	// USART0
+	// IO for USART0
 	gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
 	gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
 
@@ -27,20 +30,20 @@ void init() {
 	eclic_priority_group_set(ECLIC_PRIGROUP_LEVEL3_PRIO1);
 	eclic_irq_enable(USART0_IRQn, 1, 0);
 
-	//usart_interrupt_enable(USART0, USART_INT_TBE);
+	usart_interrupt_enable(USART0, USART_INT_TBE);
 	usart_interrupt_enable(USART0, USART_INT_RBNE);
 
 	// trun off all LEDs
 	gpio_bit_set(GPIOA, GPIO_PIN_1 | GPIO_PIN_2);
 	gpio_bit_set(GPIOC, GPIO_PIN_13);
 
-	// LED
+	// IO for LED
 	gpio_init(GPIOA, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ,
 			GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_13);
 	gpio_init(GPIOC, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_13);
 
 	usart_deinit(USART0);
-	usart_baudrate_set(USART0, 9600);
+	usart_baudrate_set(USART0, 128000);
 	usart_word_length_set(USART0, USART_WL_8BIT);
 	usart_stop_bit_set(USART0, USART_STB_1BIT);
 	usart_parity_config(USART0, USART_PM_NONE);
@@ -59,8 +62,8 @@ int _put_char(int ch) {
 }
 
 int puts(const char *str) {
-	for (char c = *str; c != '\0'; str++)
-		_put_char(c);
+	while (*str != '\0')
+		_put_char(*str++);
 }
 
 int main(int argc, const char **argv) {
@@ -68,13 +71,12 @@ int main(int argc, const char **argv) {
 
 	//gpio_bit_reset(GPIOA, GPIO_PIN_1);
 	//gpio_bit_reset(GPIOA, GPIO_PIN_2);
-	gpio_bit_reset(GPIOC, GPIO_PIN_13);
+	//gpio_bit_reset(GPIOC, GPIO_PIN_13);
 
 	for (int i = 0; i < 100; i++)
 		buf[i] = my_variable[i % 10];
 
-	for (int i = 0; i < 100; i++)
-		puts("hello, this is from the serial port\n");
+	puts("hello, this is from the serial port\r\n");
 
 	while (1);
 
